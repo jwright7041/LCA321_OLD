a5b2555cefeb22b7545ae6e274f4cc55d2c34e93
jwright7041 <jmwright120@gmail.com>
2021-04-04 21:52:16 -0500

    Values Controller

---
+++ MyFirstApi/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private string[] _values = new string[] {"value1", "value2", "value3"};
+
+        [HttpGet]
+        public IEnumerable<string> get()
+        {
+            return _values;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            id--;
+
+            if(id > 2)
+            {
+                return NotFound();
+            }
+
+            return Ok(_values[id]);
+        }
+
+    }
+}
